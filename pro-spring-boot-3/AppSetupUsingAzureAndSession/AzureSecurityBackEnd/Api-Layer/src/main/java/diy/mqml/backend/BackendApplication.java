860afbffd05aa4d2d6db31511076d813a2304721
Mark Lin <lin.pengpeng@mayo.edu>
2024-11-27 12:21:38 -0600

    adding data layer

--- pro-spring-boot-3/AppSetupUsingAzureAndSession/AzureSecurityBackEnd/Api-Layer/src/main/java/diy/mqml/backend/BackendApplication.java
+++ pro-spring-boot-3/AppSetupUsingAzureAndSession/AzureSecurityBackEnd/Api-Layer/src/main/java/diy/mqml/backend/BackendApplication.java
@@ -11,7 +11,7 @@ public class BackendApplication {
 	public static void main(String[] args) {
 
 		SpringApplication app = new SpringApplication(BackendApplication.class);
-		app.setAdditionalProfiles("security"); // app.setAdditionalProfiles("dev", "test"); allows you to specify multiple profiles.
+		app.setAdditionalProfiles("security", "data"); // app.setAdditionalProfiles("dev", "test"); allows you to specify multiple profiles.
 		app.run(args);
 	}
 
